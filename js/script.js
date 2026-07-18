3288d8e40b210db1b38bef3ef0e9a41643068c0c
1209-acgc <angiecamilagarciacuri@gmail.com>
2026-07-18 17:51:49 -0500

    Actualización Evaluación Integral

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-// Archivo JavaScript para futuras funcionalidades de KIZUNA.
+// Archivo JavaScript para futuras funcionalidades de KIZUNA
+
+// Scroll suave para los enlaces del menú
+
+document.querySelectorAll('a').forEach(link => {
+
+link.addEventListener('click', function(e){
+
+const destino = this.getAttribute('href');
+
+if(destino.startsWith("#")){
+
+e.preventDefault();
+
+document.querySelector(destino).scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+}
+
+});
+
+});
+
+// Mensaje del formulario
+
+const formularios = document.querySelectorAll("form");
+
+formularios.forEach(form => {
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("✅ Gracias por comunicarte con KIZUNA. Hemos recibido tu información.");
+
+form.reset();
+
+});
+
+});
